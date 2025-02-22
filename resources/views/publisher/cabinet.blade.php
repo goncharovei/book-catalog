a23dfa56bf8a966bf6c7ea627b82f1d6520b7f34
Eugene <goncharovei@gmail.com>
2025-02-22 12:04:57 +0300

    Layout of input Token API in the Cabinet

--- resources/views/publisher/cabinet.blade.php
+++ resources/views/publisher/cabinet.blade.php
@@ -8,13 +8,23 @@
                 <div class="card-header">{{ __('Dashboard') }}</div>
 
                 <div class="card-body">
+                    <h5 class="card-title">{{ __('API') }}</h5>
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
                         </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    <div class="col">
+                        <label for="token" class="form-label pr-2">{{ __('Token') }}</label>
+                        <div class="input-group mb-3">
+                            <input id="token" type="text" class="form-control" readonly placeholder="Token API" aria-label="Token API" aria-describedby="button-token-refresh">
+                            <button class="btn btn-outline-secondary" type="button" id="button-token-refresh">{{ __('Refresh') }}</button>
+                            <button class="btn btn-outline-secondary" type="button">{{ __('Revoke') }}</button>
+                            <button class="btn btn-outline-secondary" type="button">{{ __('Copy') }}</button>
+                        </div>
+                    </div>
+
                 </div>
             </div>
         </div>
