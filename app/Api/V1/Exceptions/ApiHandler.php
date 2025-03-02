f6aa3a630d2958101a6126fac52b031ed0ea190b
Eugene <goncharovei@gmail.com>
2025-03-02 18:45:05 +0300

    Api: send validation errors. Refactoring

--- app/Api/V1/Exceptions/ApiHandler.php
+++ app/Api/V1/Exceptions/ApiHandler.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Facades\Auth;
 use Symfony\Component\HttpFoundation\Response;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Validation\ValidationException;
 
 final readonly class ApiHandler
 {
@@ -24,25 +25,50 @@ final readonly class ApiHandler
                 return $response;
             }
 
-            Log::error(print_r([
-                'message' => $e->getMessage(),
-                'code' => [
-                    'error' => $e->getCode(),
-                    'response' => $response->getStatusCode()
-                ],
-                'file' => $e->getFile() . ' (' . $e->getLine() . ')',
-                'request' => [
-                    'user_id' => Auth::user()?->id,
-                    'uri' => $request->getUri(),
-                    'content' => $request->getContent()
-                ]
-            ], true), [__CLASS__]);
+            $this->logging($response, $e, $request);
 
-            return $response->setData(['error' => [
-                'message' => $e->getMessage(),
-                'code' => $e->getCode() ?: $response->getStatusCode()
-            ]]);
+            $errors = [
+                'response' => $this->responseError($response, $e),
+                'validation' => $this->validationErrors($e)
+            ];
+
+            return $response->setData(compact('errors'));
         });
+    }
+
+    private function responseError(Response $response, \Throwable $e): array
+    {
+        return [
+            'message' => $e->getMessage(),
+            'code' => $e->getCode() ?: $response->getStatusCode()
+        ];
+    }
+
+    private function validationErrors(\Throwable $e): array
+    {
+        if (!($e instanceof ValidationException))
+        {
+            return [];
+        }
+
+        return $e->validator->errors()->toArray();
+    }
+
+    private function logging(Response $response, \Throwable $e, Request $request): void
+    {
+        Log::error(print_r([
+            'message' => $e->getMessage(),
+            'code' => [
+                'error' => $e->getCode(),
+                'response' => $response->getStatusCode()
+            ],
+            'file' => $e->getFile() . ' (' . $e->getLine() . ')',
+            'request' => [
+                'user_id' => Auth::user()?->id,
+                'uri' => $request->getUri(),
+                'content' => $request->getContent()
+            ]
+        ], true), [__CLASS__]);
     }
 
 }
