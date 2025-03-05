e091e536eb8fa8eedbc59fc6677bf8ef7f5ccee7
Eugene <goncharovei@gmail.com>
2025-03-05 09:58:40 +0300

    Api: removed the Swagger

--- app/Api/V1/Book/Http/Resources/BookResource.php
+++ app/Api/V1/Book/Http/Resources/BookResource.php
@@ -6,47 +6,7 @@ use App\Common\Models\Book;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
-/**
- * Class BookResource
- * @package App\Api\V1\Book\Http\Resources
- *
- * @OA\Schema(
- *   schema="Book",
- *   title="Book",
- *   @OA\Property(
- *       property="id",
- *       type="integer",
- *      format="int64"
- *   ),
- *   @OA\Property(
- *       property="isbn",
- *       type="string"
- *   ),
- *   @OA\Property(
- *        property="name",
- *        type="string"
- *    ),
- *     @OA\Property(
-*         property="author_names",
-*         type="array",
-*         @OA\Items(ref="#/components/schemas/NameAuthor")
-*      ),
- *     @OA\Property(
- *      property="year_publication",
- *      type="date",
- *      format="Y"
- *     ),
- *     @OA\Property(
- *       property="detail_link",
- *       type="string"
- *      )
- *  )
- * @OA\Schema(
- *    schema="NameAuthor",
- *    title="Name of Author",
- *    type="string"
- * )
- */
+
 class BookResource extends JsonResource
 {
     /**
