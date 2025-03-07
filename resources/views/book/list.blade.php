526cd01d62b2a8c7d735292d056dd67417076efb
Eugene <goncharovei@gmail.com>
2025-03-07 18:25:03 +0300

    Front: include static files of DataTables

--- resources/views/book/list.blade.php
+++ resources/views/book/list.blade.php
@@ -502,13 +502,12 @@
 @endsection
 
 @push('styles')
-    <link href="https://cdn.datatables.net/v/dt/jszip-3.10.1/dt-2.2.0/af-2.7.0/b-3.2.0/b-colvis-3.2.0/b-html5-3.2.0/b-print-3.2.0/cr-2.0.4/date-1.5.5/fc-5.0.4/fh-4.0.1/kt-2.12.1/r-3.0.3/rg-1.5.1/rr-1.5.0/sc-2.4.3/sb-1.8.1/sp-2.3.3/sl-3.0.0/sr-1.4.1/datatables.min.css" rel="stylesheet">
+    <link href="{{ asset('assets/libs/datatables/datatables.min.css') }}" rel="stylesheet"/>
     @vite('resources/pages/home/styles.css')
 @endpush
 
 @push('scripts')
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/pdfmake.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/vfs_fonts.js"></script>
-    <script src="https://cdn.datatables.net/v/dt/jszip-3.10.1/dt-2.2.0/af-2.7.0/b-3.2.0/b-colvis-3.2.0/b-html5-3.2.0/b-print-3.2.0/cr-2.0.4/date-1.5.5/fc-5.0.4/fh-4.0.1/kt-2.12.1/r-3.0.3/rg-1.5.1/rr-1.5.0/sc-2.4.3/sb-1.8.1/sp-2.3.3/sl-3.0.0/sr-1.4.1/datatables.min.js"></script>
+
+    <script src="{{ asset('assets/libs/datatables/datatables.min.js') }}"></script>
     @vite('resources/pages/home/script.js')
 @endpush
