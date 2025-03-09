6ca465af9023407b09b52ab0dd65d7d36a81af94
Eugene <goncharovei@gmail.com>
2025-03-09 16:34:33 +0300

    Front, catalog: refactoring

--- resources/pages/home/script.js
+++ resources/pages/home/script.js
@@ -1,27 +1,40 @@
 $(document).ready(function()
 {
-    let $dataTable = new DataTable('#catalog', {
-        ajax: window.bookDataTableUrl,
-        processing: true,
-        serverSide: true,
-        responsive: true,
-        layout: {
-            topStart: {
-                buttons: ['copy', 'csv', 'excel', 'pdf', 'print']
-            },
-            bottomStart: {
-                buttons: ['pageLength']
+    const Catalog = function () {
+        let dataTable = null;
+
+        let datatableCreate = function () {
+            dataTable = new DataTable('#catalog', {
+                ajax: window.bookDataTableUrl,
+                processing: true,
+                serverSide: true,
+                responsive: true,
+                layout: {
+                    topStart: {
+                        buttons: ['copy', 'csv', 'excel', 'pdf', 'print']
+                    },
+                    bottom2Start: {
+                        buttons: ['pageLength']
+                    }
+                },
+                pageLength: 10,
+                lengthMenu: [10, 25, 50, 100, { label: 'All', value: -1 }],
+                columns: [
+                    {data: 'name'},
+                    {data: 'author_names'},
+                    {data: 'isbn'},
+                    {data: 'year_publication'},
+                    {data: 'publisher_name'}
+                ]
+            });
+        }
+
+        return {
+            init: function (){
+                datatableCreate();
             }
-        },
-        pageLength: 10,
-        lengthMenu: [10, 25, 50, 100, { label: 'All', value: -1 }],
-        columns: [
-            {data: 'name'},
-            {data: 'author_names'},
-            {data: 'isbn'},
-            {data: 'year_publication'},
-            {data: 'publisher_name'}
-        ]
-    });
+        }
+    }();
+    Catalog.init();
 
 })
