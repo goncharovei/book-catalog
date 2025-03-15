bce71daa9dfab6007f7a33faa6ef416ab7c1c8dd
Eugene <goncharovei@gmail.com>
2025-03-15 21:01:46 +0300

    Test: mail notification

--- tests/Feature/Front/AuthTest.php
+++ tests/Feature/Front/AuthTest.php
@@ -2,11 +2,14 @@
 
 namespace Tests\Feature\Front;
 
+use App\Common\Models\Publisher;
+use App\Front\Publisher\Listeners\PublisherRegisteredListener;
 use App\Front\Publisher\Mail\PublisherRegisteredMail;
 use App\Front\Publisher\Notifications\PublisherVerifyEmailNotification;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Event;
+use Illuminate\Support\Facades\Lang;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Notification;
 use Illuminate\Support\Facades\Queue;
@@ -42,7 +45,10 @@ class AuthTest extends TestCaseFront
         $this->assertAuthenticated();
 
         Event::assertDispatched(Registered::class);
-
+        Event::assertListening(
+            Registered::class,
+            PublisherRegisteredListener::class
+        );
         Mail::assertNotSent(PublisherRegisteredMail::class);
         Notification::assertNotSentTo(Auth::user(), PublisherVerifyEmailNotification::class);
         Queue::assertNothingPushed();
@@ -50,4 +56,34 @@ class AuthTest extends TestCaseFront
         $response->assertRedirect();
     }
 
+    public function testPublisherRegisteredMail(): void
+    {
+        Mail::fake();
+
+        $publisher = new Publisher([
+            'name' => fake()->name(),
+            'email' => fake()->unique()->safeEmail(),
+        ]);
+
+        $letter = new PublisherRegisteredMail($publisher);
+        $letter->assertSeeInHtml($publisher['name']);
+        $letter->assertSeeInHtml($publisher['email']);
+
+        Mail::to(config('app.admin_email'))->send($letter);
+        Mail::assertQueued(PublisherRegisteredMail::class, function ($mail) {
+            return $mail->hasTo(config('app.admin_email'));
+        });
+    }
+
+    public function testPublisherVerifyEmailNotification(): void
+    {
+        Notification::fake();
+
+        $publisher = Publisher::factory()->create();
+        $notification = new PublisherVerifyEmailNotification();
+        $rendered = $notification->toMail($publisher)->render();
+
+        $this->assertStringContainsString(Lang::get('Verify Email Address'), $rendered);
+    }
 }
+
